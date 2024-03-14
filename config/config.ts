1242f4ef43b1409c642bb03e34a4a6762bcd0af8
Jacqueline Zhang <jacqueline.zhang@vungle.com>
2024-03-14 17:52:36 +0800

    add automation test

--- config/config.ts
+++ config/config.ts
@@ -1,54 +1,25 @@
-import "dotenv/config";
-import {
-  Hex,
-  createWalletClient,
-  http,
-  PrivateKeyAccount,
-} from "viem";
+import { Hex, http, Address } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
 
 import { sepolia } from "viem/chains";
 
-export const getStoryConfig = (account: PrivateKeyAccount) => ({
-  transport: http(RPC_URL),
-  account,
-}) as StoryConfig;
+export const transport = http(process.env.RPC_PROVIDER_URL);
+export const privateKeyA = process.env.WALLET_PRIVATE_KEY_A as Hex;
+export const privateKeyB = process.env.WALLET_PRIVATE_KEY_B as Hex;
 
-export const TOKEN_CONTRACT_ADDRESS = "0x7ee32b8B515dEE0Ba2F25f612A04a731eEc24F49";
-export const TEST_WALLET_A_ADDRESS = (process.env.TEST_WALLET_A_ADDRESS || "0x") as `0x${string}`;
-export const TEST_WALLET_B_ADDRESS = (process.env.TEST_WALLET_B_ADDRESS || "0x") as `0x${string}`;
-export const TEST_WALLET_C_ADDRESS = (process.env.TEST_WALLET_C_ADDRESS || "0x") as `0x${string}`;
-export const NFT_CONTRACT_ADDRESS = (process.env.NFT_CONTRACT_ADDRESS || "0x") as `0x${string}`;
-export const RPC_URL = process.env.RPC_URL || "";
-export const LICENSE_MODULE = (process.env.LICENSE_MODULE || "") as `0x${string}`;
-export const MINT_FEE_TOKEN = (process.env.MINT_FEE_TOKEN || "") as `0x${string}`;
-export const ROYALTY_POLICY = (process.env.ROYALTY_POLICY || "") as `0x${string}`;
+export const accountA = privateKeyToAccount(privateKeyA as Address);
+export const accountB = privateKeyToAccount(privateKeyB as Address);
 
-export const accountA = privateKeyToAccount((process.env.PRIVATE_KEYS?.split(',')[0] || '0x') as Hex);
-export const accountB = privateKeyToAccount((process.env.PRIVATE_KEYS?.split(',')[1] || '0x') as Hex);
-export const accountC = privateKeyToAccount((process.env.PRIVATE_KEYS?.split(',')[2] || '0x') as Hex);
-
-const configA: StoryConfig = getStoryConfig(accountA);
-const configB: StoryConfig = getStoryConfig(accountB);
-const configC: StoryConfig = getStoryConfig(accountC);
-
-export const storyClientA: StoryClient = StoryClient.newClient(configA);
-export const storyClientB: StoryClient = StoryClient.newClient(configB);
-export const storyClientC: StoryClient = StoryClient.newClient(configC);
-
-export const walletClientA = createWalletClient({
-  transport: http(RPC_URL),
-  chain: sepolia,
+export const configA: StoryConfig = {
   account: accountA,
-});
-export const walletClientB = createWalletClient({
-  transport: http(RPC_URL),
-  chain: sepolia,
+  transport: transport,
+}
+
+export const configB: StoryConfig = {
   account: accountB,
-});
-export const walletClientC = createWalletClient({
-  transport: http(RPC_URL),
-  chain: sepolia,
-  account: accountC,
-});
+  transport: transport,
+}
+
+export const clientA = StoryClient.newClient(configA)
+export const clientB = StoryClient.newClient(configB)
