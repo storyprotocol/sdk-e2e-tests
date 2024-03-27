174542211aed6ffd6c7701357c00f1150d4e33d9
Jacqueline Zhang <jacqueline.zhang@vungle.com>
2024-03-27 19:40:02 +0800

    add more e2e flow and refactor

--- config/config.ts
+++ config/config.ts
@@ -4,14 +4,18 @@ import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
 
 export const licenseModuleAddress = process.env.LICENSE_MODULE_ADDRESS as Hex;
 export const nftContractAddress = process.env.MY_NFT_CONTRACT_ADDRESS as Hex;
+export const royaltyPolicyAddress = process.env.ROYALTY_POLICY_ADDRESS as Hex;
+export const mintFeeToken = process.env.MINT_FEE_TOKEN as Hex;
 export const transport = http(process.env.RPC_PROVIDER_URL);
 export const privateKeyA = process.env.WALLET_PRIVATE_KEY_A as Hex;
 export const privateKeyB = process.env.WALLET_PRIVATE_KEY_B as Hex;
 export const privateKeyC = process.env.WALLET_PRIVATE_KEY_C as Hex;
+export const privateKeyD = process.env.WALLET_PRIVATE_KEY_D as Hex;
 
 export const accountA = privateKeyToAccount(privateKeyA as Address);
 export const accountB = privateKeyToAccount(privateKeyB as Address);
 export const accountC = privateKeyToAccount(privateKeyC as Address);
+export const accountD = privateKeyToAccount(privateKeyD as Address);
 
 export const configA: StoryConfig = {
   account: accountA,
@@ -28,6 +32,12 @@ export const configC: StoryConfig = {
   transport: transport,
 }
 
+export const configD: StoryConfig = {
+  account: accountD,
+  transport: transport,
+}
+
 export const clientA = StoryClient.newClient(configA)
 export const clientB = StoryClient.newClient(configB)
 export const clientC = StoryClient.newClient(configC)
+export const clientD = StoryClient.newClient(configD)
--- test/e2e/setPermissions.test.ts
+++ test/e2e/setPermissions.test.ts
@@ -1,11 +1,12 @@
-import { privateKeyA, accountB, licenseModuleAddress, accountA } from '../../config/config'
-import { mintNFT } from '../../utils/utils'
+import { privateKeyA, accountB, accountA, licenseModuleAddress, nftContractAddress} from '../../config/config'
+import { mintNFT, captureConsoleLogs } from '../../utils/utils'
 import { registerRootIp, mintLicense, setPermission } from '../../utils/sdkUtils'
 import { expect } from 'chai'
 
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 chai.use(chaiAsPromised);
+import addContext = require("mochawesome/addContext");
 
 let tokenIdA: any
 let ipIdA: any
@@ -14,21 +15,34 @@ const waitForTransaction: boolean = true;
 
 describe('SDK E2E Test', function () {
     describe("Set Permissions Functions", async function () {
+        // To print test results in test report
+        let consoleLogs: string[] = [];
+        beforeEach(function () {
+            consoleLogs = captureConsoleLogs(consoleLogs)
+        });
+
+        afterEach(function () {
+            if (consoleLogs.length > 0) {
+                addContext(this, {
+                    title: 'Test Result',
+                    value: consoleLogs[0]
+                });
+            }
+        });
+
         describe("Set permission - 1 ALLOW", async function (){
             step("Mint a NFT to Wallet A and get a tokenId (tokenIdA)", async function () {
                 tokenIdA = await mintNFT(privateKeyA);
                 expect(tokenIdA).not.empty
             });
         
-            step("Wallet A register a root IP Asset with tokenIdA, get an ipId (ipIdA)", async function () {
+            step("Wallet A register a root IP Asset without policy, get an ipId (ipIdA)", async function () {
                 const response = await expect(
-                    registerRootIp("A", "1", tokenIdA, waitForTransaction)
+                    registerRootIp("A", "0", nftContractAddress, tokenIdA, waitForTransaction)
                 ).to.not.be.rejected
         
-                expect(response.txHash).to.be.a("string");
-                expect(response.txHash).not.empty;
-                expect(response.ipId).to.be.a("string");
-                expect(response.ipId).not.empty;
+                expect(response.txHash).to.be.a("string").and.not.empty;
+                expect(response.ipId).to.be.a("string").and.not.empty
         
                 ipIdA = response.ipId
             });
@@ -38,38 +52,33 @@ describe('SDK E2E Test', function () {
                     setPermission("A", ipIdA, accountB.address, licenseModuleAddress, 1, waitForTransaction)
                 ).to.not.be.rejected;
         
-                expect(response.txHash).to.be.a("string");
-                expect(response.txHash).not.empty;
+                expect(response.txHash).to.be.a("string").and.not.empty;
                 expect(response.success).to.be.true
             });
         
             step("Wallet B can mint a license with ipIdA", async function () {
                 const response = await expect(
-                    mintLicense("B", "1", ipIdA, accountA.address, waitForTransaction)
+                    mintLicense("B", "1", ipIdA, 2, accountA.address, waitForTransaction)
                 ).to.not.be.rejected;
         
-                expect(response.txHash).to.be.a("string");
-                expect(response.txHash).not.empty;
-                expect(response.licenseId).to.be.a("string");
-                expect(response.licenseId).not.empty;
+                expect(response.txHash).to.be.a("string").and.not.empty;
+                expect(response.licenseId).to.be.a("string").and.not.empty;
             });
         });
-    
+        
         describe("Set permission - 2 DENY", async function (){
             step("Mint a NFT to Wallet A and get a tokenId (tokenIdA)", async function () {
                 tokenIdA = await mintNFT(privateKeyA);
                 expect(tokenIdA).not.empty
             });
         
-            step("Wallet A register a root IP Asset with tokenIdA, get an ipId (ipIdB)", async function () {
+            step("Wallet A register a root IP Asset without policy, get an ipId (ipIdA)", async function () {
                 const response = await expect(
-                    registerRootIp("A", "1", tokenIdA, waitForTransaction)
+                    registerRootIp("A", "0", nftContractAddress, tokenIdA, waitForTransaction)
                 ).to.not.be.rejected
         
-                expect(response.txHash).to.be.a("string");
-                expect(response.txHash).not.empty;
-                expect(response.ipId).to.be.a("string");
-                expect(response.ipId).not.empty;
+                expect(response.txHash).to.be.a("string").and.not.empty;
+                expect(response.ipId).to.be.a("string").and.not.empty;
         
                 ipIdA = response.ipId
             });
@@ -78,18 +87,17 @@ describe('SDK E2E Test', function () {
                 const response = await expect(
                     setPermission("A", ipIdA, accountB.address, licenseModuleAddress, 2, waitForTransaction)
                 ).to.not.be.rejected;
-        
-                expect(response.txHash).to.be.a("string");
-                expect(response.txHash).not.empty;
+
+                expect(response.txHash).to.be.a("string").and.not.empty;
                 expect(response.success).to.be.true
             });
-        
+            
             step("Wallet B can NOT mint a license with ipIdA", async function () {
                 const response = await expect(
-                    mintLicense("B", "1", ipIdA, accountA.address, waitForTransaction)
-                ).to.be.rejected;
+                    mintLicense("B", "1", ipIdA, 1, accountA.address, waitForTransaction)
+                ).to.be.rejectedWith("Failed to mint license: The contract function \"mintLicense\" reverted.",
+                                    "Error: LicensingModule__CallerNotLicensorAndPolicyNotSet()")
             });
         });
     });
 });
-
