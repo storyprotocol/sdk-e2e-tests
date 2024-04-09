863bd53ccdae93053faa46dc13d07fbf44139f30
Jacqueline Zhang <jacqueline@57blocks.com>
2024-04-09 23:49:17 +0800

    configure 2 test env

--- config/config.ts
+++ config/config.ts
@@ -1,12 +1,33 @@
-import { Hex, http, Address } from "viem";
+import { Hex, http, Address, Chain, defineChain } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { StoryClient, StoryConfig } from "@story-protocol/core-sdk";
+import { SupportedChainIds } from "@story-protocol/core-sdk/dist/declarations/src/types/config";
 
-export const licenseModuleAddress = process.env.LICENSE_MODULE_ADDRESS as Hex;
-export const nftContractAddress = process.env.MY_NFT_CONTRACT_ADDRESS as Hex;
-export const royaltyPolicyAddress = process.env.ROYALTY_POLICY_ADDRESS as Hex;
-export const mintFeeTokenAddress = process.env.MINT_FEE_TOKEN as Hex;
-export const transport = http(process.env.RPC_PROVIDER_URL);
+const TEST_ENV = process.env.TEST_ENV as SupportedChainIds
+console.log(TEST_ENV)
+
+export let licenseModuleAddress: Hex;
+export let nftContractAddress: Hex;
+export let royaltyPolicyAddress: Hex;
+export let mintFeeTokenAddress: Hex;
+export let rpcProviderUrl: any;
+
+if (TEST_ENV == "sepolia") {
+  rpcProviderUrl = http(process.env.SEPOLIA_RPC_PROVIDER_URL);
+  licenseModuleAddress = process.env.SEPOLIA_LICENSE_MODULE_ADDRESS as Hex;
+  nftContractAddress = process.env.SEPOLIA_NFT_CONTRACT_ADDRESS as Hex;
+  royaltyPolicyAddress = process.env.SEPOLIA_ROYALTY_POLICY_ADDRESS as Hex;
+  mintFeeTokenAddress = process.env.SEPOLIA_MINT_FEE_TOKEN as Hex;
+} else if (TEST_ENV == "storyTestnet") {
+  rpcProviderUrl = http(process.env.STORY_RPC_PROVIDER_URL);
+  licenseModuleAddress = process.env.STORY_LICENSE_MODULE_ADDRESS as Hex;
+  nftContractAddress = process.env.STORY_NFT_CONTRACT_ADDRESS as Hex;
+  royaltyPolicyAddress = process.env.STORY_ROYALTY_POLICY_ADDRESS as Hex;
+  mintFeeTokenAddress = process.env.STORY_MINT_FEE_TOKEN as Hex;
+} else {
+  throw new Error(`Unknown TEST_ENV value: ${TEST_ENV}`);
+}
+
 export const privateKeyA = process.env.WALLET_PRIVATE_KEY_A as Hex;
 export const privateKeyB = process.env.WALLET_PRIVATE_KEY_B as Hex;
 export const privateKeyC = process.env.WALLET_PRIVATE_KEY_C as Hex;
@@ -17,19 +38,54 @@ export const accountC = privateKeyToAccount(privateKeyC as Address);
 
 export const configA: StoryConfig = {
   account: accountA,
-  transport: transport,
+  chainId: TEST_ENV,
+  transport: rpcProviderUrl,
 }
 
 export const configB: StoryConfig = {
   account: accountB,
-  transport: transport,
+  chainId: TEST_ENV,
+  transport: rpcProviderUrl,
 }
 
 export const configC: StoryConfig = {
   account: accountC,
-  transport: transport,
+  chainId: TEST_ENV,
+  transport: rpcProviderUrl,
 }
 
 export const clientA = StoryClient.newClient(configA)
 export const clientB = StoryClient.newClient(configB)
 export const clientC = StoryClient.newClient(configC)
+
+export function chainStringToViemChain(chainId: SupportedChainIds): Chain {
+  switch (chainId) {
+    case "sepolia":
+    case "storyTestnet":
+      return storyTestnet;
+    default:
+      throw new Error(`chainId ${chainId as string} not supported`);
+  }
+}
+  
+export const storyTestnet = defineChain({
+  id: 15_13,
+  name: "story-network",
+  nativeCurrency: { name: "Ether", symbol: "SEP", decimals: 18 },
+  rpcUrls: {
+  default: {
+    http: ["https://story-network.rpc.caldera.xyz/http"],
+    webSocket: ["wss://story-network.rpc.caldera.xyz/ws"],
+    }
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://story-network.explorer.caldera.xyz" },
+  },
+  contracts: {
+    multicall3: {
+      address: "0xcA11bde05977b3631167028862bE2a173976CA11",
+      blockCreated: 5882,
+    },
+  },
+  testnet: true
+});
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,7 +1,20 @@
-import { Hex, http, Address, createWalletClient, createPublicClient } from 'viem'
+import { Hex, http, Address, createWalletClient, createPublicClient, Chain } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import { sepolia } from 'viem/chains';
 import fs from 'fs';
+import { chainStringToViemChain, nftContractAddress, rpcProviderUrl } from "../config/config";
+
+const TEST_ENV = process.env.TEST_ENV as String
+console.log(TEST_ENV)
+
+let chainId: Chain
+if (TEST_ENV == "sepolia") {
+  chainId = sepolia
+} else if (TEST_ENV == "storyTestnet") {
+  chainId = chainStringToViemChain("storyTestnet")
+} else {
+  throw new Error(`Unknown TEST_ENV value: ${TEST_ENV}`);
+}
 
 export function sleep(second: number) {
   return new Promise((resolve) => setTimeout(resolve, second * 1000));
@@ -25,18 +38,17 @@ export function captureConsoleLogs(consoleLogs:string[]){
   return consoleLogs
 }
 
-
 export async function mintNFT(WALLET_PRIVATE_KEY: Hex): Promise<string> {
   const account = privateKeyToAccount(WALLET_PRIVATE_KEY as Address)
+  const baseConfig = {
+    chain: chainId,
+    transport: http(rpcProviderUrl)    
+  }
   const walletClient = createWalletClient({
-    account,
-    chain: sepolia,
-    transport: http(),
+    ...baseConfig,
+    account
   })
-  const publicClient = createPublicClient({
-    chain: sepolia,
-    transport: http()
-  })
+  const publicClient = createPublicClient(baseConfig)
   const contractAbi = {
     inputs: [{ internalType: 'address', name: 'to', type: 'address' }],
     name: 'mint',
@@ -58,6 +70,57 @@ export async function mintNFT(WALLET_PRIVATE_KEY: Hex): Promise<string> {
     address: process.env.MY_NFT_CONTRACT_ADDRESS as Address,
     functionName: 'mint',
     args: [account.address],
+    abi: [contractAbi]
+  })
+
+  const { logs } = await publicClient.waitForTransactionReceipt({
+    hash: hash,
+  });
+
+  let tokenId: any
+  if (logs[0].topics[3]) {
+    tokenId = parseInt(logs[0].topics[3], 16);
+  }
+
+  console.log(`Minted NFT successful with hash: ` + JSON.stringify(hash) + `\nMinted NFT tokenId: ` + JSON.stringify(tokenId));
+  return String(tokenId);
+}
+
+export async function mintNFTWithTokenID(WALLET_PRIVATE_KEY: Hex, id: number): Promise<string> {
+  const account = privateKeyToAccount(WALLET_PRIVATE_KEY as Address)
+  const baseConfig = {
+    chain: chainId,
+    transport: http(rpcProviderUrl)    
+  }
+  const walletClient = createWalletClient({
+    ...baseConfig,
+    account
+  })
+  const publicClient = createPublicClient(baseConfig)
+  const contractAbi = {
+    inputs: [
+      { internalType: 'address', name: 'to', type: 'address' },
+      { internalType: "uint256", name: "tokenId",type: "uint256" }
+    ],
+    name: 'mintId',
+    outputs: [
+      { internalType: 'uint256', name: 'tokenId', type: 'uint256' },
+    ],
+    stateMutability: 'nonpayable',
+    type: 'function',
+  }
+
+  // 3. Mint an NFT to your account
+  const { result } = await publicClient.simulateContract({
+    address: nftContractAddress as Address,
+    functionName: 'mintId',
+    args: [account.address, BigInt(id)],
+    abi: [contractAbi]
+  })
+  const hash = await walletClient.writeContract({
+    address: nftContractAddress as Address,
+    functionName: 'mintId',
+    args: [account.address, BigInt(id)],
     abi: [contractAbi]
   })
 
