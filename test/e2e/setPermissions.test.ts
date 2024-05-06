78a057bee5365e324a65b518783ba444967aa3d2
Jacqueline Zhang <jacqueline.zhang@vungle.com>
2024-05-06 17:34:41 +0800

    add more comments

--- test/e2e/setPermissions.test.ts
+++ test/e2e/setPermissions.test.ts
@@ -1,6 +1,6 @@
 import { privateKeyA, accountB, licensingModuleAddress, nftContractAddress, mintingFeeTokenAddress} from '../../config/config';
-import { mintNFTWithRetry } from '../../utils/utils';
-import { registerNonComSocialRemixingPIL, registerCommercialUsePIL, registerIpAsset, setPermission, attachLicenseTerms } from '../../utils/sdkUtils';
+import { checkMintResult, mintNFTWithRetry } from '../../utils/utils';
+import { registerNonComSocialRemixingPIL, registerCommercialUsePIL, registerIpAsset, setPermission, attachLicenseTerms, registerDerivative, mintLicenseTokens } from '../../utils/sdkUtils';
 import { expect } from 'chai';
 import { Hex } from 'viem';
 import '../setup';
@@ -20,7 +20,7 @@ const waitForTransaction: boolean = true;
 
 describe('SDK E2E Test', function () {
     describe("[smoke]Set Permissions", async function () {
-        this.beforeAll("Register 2 license terms and 2 IP assets", async function () {
+        this.beforeAll("Wallet A register 2 license terms and 2 IP assets", async function () {
             const responselicenseTerms1 = await expect(
                 registerNonComSocialRemixingPIL("A", waitForTransaction)
             ).to.not.be.rejected;
@@ -34,10 +34,7 @@ describe('SDK E2E Test', function () {
             licenseTermsId2 = responselicenseTerms2.licenseTermsId;
                         
             tokenIdA = await mintNFTWithRetry(privateKeyA);
-            if (tokenIdA === '') {
-                throw new Error('Unable to mint NFT');
-            }
-            expect(tokenIdA).not.empty;
+            checkMintResult(tokenIdA);
         
             const responseRegisterIpA = await expect(
                 registerIpAsset("A", nftContractAddress, tokenIdA, waitForTransaction)
@@ -47,12 +44,9 @@ describe('SDK E2E Test', function () {
             expect(responseRegisterIpA.ipId).to.be.a("string").and.not.empty;
     
             ipIdA = responseRegisterIpA.ipId;
-                        
+
             tokenIdB = await mintNFTWithRetry(privateKeyA);
-            if (tokenIdB === '') {
-                throw new Error('Unable to mint NFT');
-            }
-            expect(tokenIdB).not.empty;
+            checkMintResult(tokenIdB);
         
             const responseRegisterIpB = await expect(
                 registerIpAsset("A", nftContractAddress, tokenIdB, waitForTransaction)
@@ -63,8 +57,16 @@ describe('SDK E2E Test', function () {
     
             ipIdB = responseRegisterIpB.ipId;
         })
-
+        
         describe("Set permission - 1 ALLOW", async function (){
+            // error 0xb3e96921: AccessController__PermissionDenied(address,address,address,bytes4)
+            step("Wallet B(non-owner) can NOT attach licenseTermsId1 to ipIdA", async function () {
+                const response = await expect(
+                    attachLicenseTerms("B", ipIdA, licenseTermsId1, waitForTransaction)
+                ).to.be.rejectedWith("Failed to attach license terms: The contract function \"attachLicenseTerms\" reverted with the following signature:", "0xb3e96921");
+            });            
+
+            // wallet B is granted permission - 1 ALLOW for ipIdA 
             step("Wallet A set permission (permission id: 1) to allow Wallet B to call license Module for ipIdA", async function () {
                 const response = await expect(
                     setPermission("A", ipIdA, accountB.address, licensingModuleAddress, 1, waitForTransaction)
@@ -82,6 +84,8 @@ describe('SDK E2E Test', function () {
                 expect(response.txHash).to.be.a("string").and.not.empty;
             });
 
+            // wallet B is not granted any permission for ipIdB
+            // error 0xb3e96921: AccessController__PermissionDenied(address,address,address,bytes4)
             step("Wallet B can NOT attach licenseTermsId1 to ipIdB", async function () {
                 const response = await expect(
                     attachLicenseTerms("B", ipIdB, licenseTermsId1, waitForTransaction)
@@ -90,6 +94,7 @@ describe('SDK E2E Test', function () {
         });
         
         describe("Set permission - 2 DENY", async function (){                            
+            // change wallet B's permission to 2 DENY for ipIdA
             step("Wallet A set permission (permission id: 2) to NOT allow Wallet B to call license Module for ipIdA", async function () {
                 const response = await expect(
                     setPermission("A", ipIdA, accountB.address, licensingModuleAddress, 2, waitForTransaction)
@@ -99,6 +104,7 @@ describe('SDK E2E Test', function () {
                 expect(response.success).to.be.true;
             });
             
+            // error 0xb3e96921: AccessController__PermissionDenied(address,address,address,bytes4)
             step("Wallet B can NOT attach licenseTermsId1 to ipIdA", async function () {
                 const response = await expect(
                     attachLicenseTerms("B", ipIdA, licenseTermsId2, waitForTransaction)
@@ -107,6 +113,7 @@ describe('SDK E2E Test', function () {
         });
         
         describe("Set permission - 0 ABSTAIN", async function (){
+            // wallet B is granted permission - 1 ALLOW for ipIdB
             step("Wallet A set permission (permission id: 1) to allow Wallet B to call license Module for ipIdB", async function () {
                 const response = await expect(
                     setPermission("A", ipIdB, accountB.address, licensingModuleAddress, 1, waitForTransaction)
@@ -124,6 +131,7 @@ describe('SDK E2E Test', function () {
                 expect(response.txHash).to.be.a("string").and.not.empty;
             });
             
+            // change wallet B's permission to 0 ABSTAIN for ipIdB 
             step("Wallet A set permission (permission id: 0) to NOT allow Wallet B to call license Module for ipIdB", async function () {
                 const response = await expect(
                     setPermission("A", ipIdB, accountB.address, licensingModuleAddress, 0, waitForTransaction)
@@ -133,6 +141,7 @@ describe('SDK E2E Test', function () {
                 expect(response.success).to.be.true;
             });
             
+            // error 0xb3e96921: AccessController__PermissionDenied(address,address,address,bytes4)
             step("Wallet B can NOT attach licenseTermsId2 to ipIdB", async function () {
                 const response = await expect(
                     attachLicenseTerms("B", ipIdB, licenseTermsId2, waitForTransaction)
